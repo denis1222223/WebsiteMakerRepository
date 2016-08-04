17dbe985e848d44709278550e4d63009b9ae20cd
vladlipski <www.vlad.by55555@gmail.com>
2016-08-04 19:33:16 +0300

    Test create form
    Проверка формы с селектом и с чекбоксами

---
+++ CourseProject/CourseProject/Controllers/SiteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseProject.Controllers
+{
+    public class SiteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Create()
+        {
+            return "Спасибо, " + Request.Form.Keys + ", за покупку!";
+        }
+    }
+}
